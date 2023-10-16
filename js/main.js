b5a2a8a9f27699bd0129dc1edf4480bc247fae04
edgarsmf2603 <edgarsmfp26@gmail.com>
2023-10-16 01:19:40 -0400

    Login localStorage

--- js/main.js
+++ js/main.js
@@ -67,13 +67,41 @@ $(function () {
     theme.attr("href", "css/blue.css");
   });
 
-  // scroll ascend
+  // Scroll Ascend
   $('.ascend').click(function (e) {
     e.preventDefault();
     $('html, body').animate({
       scrollTop: 0
     }, 500);
-    
+
     return false;
   });
+
+  // Login false
+
+  $("#login form").submit(function () {
+    var form_name = $("#form_name").val();
+
+    localStorage.setItem("form_name", form_name);
+
+  });
+
+  var form_name = localStorage.getItem("form_name");
+
+  if(form_name != null && form_name != "undefined" ){
+    var about_parrafo = $("#about p");
+
+    about_parrafo.html("<br><strong>Bienvenido, " + form_name + "</strong>");
+    about_parrafo.append("<br><a href='#' id='logout'>Cerrar sesion</a>");
+
+    $("#login").hide();
+
+    $("#logout").click(function(){
+      localStorage.clear();
+      location.reload();
+    });
+  }
+  
+
+
 });
